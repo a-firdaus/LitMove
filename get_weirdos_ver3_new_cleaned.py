940df203eb699f06dd22c0b1bcf33cff088b34d3
a-firdaus <azka.firdaus@rwth-aachen.de>
2023-12-31 11:27:53 +0700

    fcn get_distance_litoli

--- get_weirdos_ver3_new_cleaned.py
+++ get_weirdos_ver3_new_cleaned.py
@@ -6932,3 +6932,103 @@ def plot_amount_type(dataframe, litype, el, style):
 # #         Li_idx_coor_dict[index] = (x, y, z)
 
 # #     return Li_idx_coor_dict
+
+
+##############################################################################################################################################################
+##############################################################################################################################################################
+##############################################################################################################################################################
+##############################################################################################################################################################
+##############################################################################################################################################################
+
+
+def get_distance_litoli(dataframe, max_mapping_radius, destination_directory, idx_file_group, idx_ref, var_filename):
+    """
+        idx_file_group = [idx_init, idx_end]
+    """
+    df_distance = pd.DataFrame()
+    coor_Li_ref = []
+    # df_dist_litoli = pd.DataFrame()
+
+    # df_dist_litoli["dist"] = None
+
+    file_ref = f"{int(dataframe['geometry'][idx_ref])}_{int(dataframe['path'][idx_ref])}_{var_filename}.cif"
+    file_path_ref = os.path.join(destination_directory, file_ref)
+
+    structure_ref = Structure.from_file(file_path_ref)
+
+    for idx, coor in enumerate(structure_ref):
+        if coor.species_string == "Li":
+            coor_Li_ref.append(coor.frac_coords)
+
+    # for i in path_geo:
+    dataframe_group = dataframe[idx_file_group[0]:idx_file_group[1]]
+    # if idx_ref > idx_file_group[1]:
+    #     # dataframe_group = dataframe_group.append(dataframe[idx_ref-1:idx_ref], ignore_index=True)
+    #     dataframe_group = pd.concat([dataframe[idx_ref-1:idx_ref], dataframe[idx_file_group[0]:idx_file_group[1]]], ignore_index=True)
+
+    for index in range(dataframe_group["geometry"].size):
+        # for index in [1]:
+        coor_Li = []
+        dict_distance = defaultdict(list)
+
+        file = f"{int(dataframe_group['geometry'][index])}_{int(dataframe_group['path'][index])}_{var_filename}.cif"
+        file_path = os.path.join(destination_directory, file)
+
+        structure = Structure.from_file(file_path)
+        # frac_coor = structure.frac_coords
+
+        for idx, coor in enumerate(structure):
+            if coor.species_string == "Li":
+                coor_Li.append(coor.frac_coords)        
+            
+        coors_Li_dist_structures = defaultdict(list)
+
+        for j in range(len(coor_Li)):
+            df_distance.at[index, f"{j}"] = None  
+
+            distance = mic_eucledian_distance(coor_Li_ref[j], coor_Li[j])
+
+            dict_distance[f"{j}"] = {f'dist: {distance}, coor_ref: {coor_Li_ref[j]}, coor_Li: {coor_Li[j]}'}
+            df_distance.at[index, f"{j}"] = distance
+
+            diameter_24g48h = max_mapping_radius * 2
+            if distance < diameter_24g48h and index != idx_ref:
+                print(f"path: {index}, Li: {j}, distance: {distance}")
+
+    #         coors_Li_dist_structures_dict = {}
+            
+    #         for k in range(len(coor_Li)):
+
+    #             distance_litoli = mic_eucledian_distance(coor_Li[j], coor_Li[k])
+
+    #             coors_Li_dist_structures_dict['coors'] = (coor_Li[j], coor_Li[k])
+    #             coors_Li_dist_structures_dict['dist'] = distance_litoli
+    #             # coors_Li_dist_structures_dict['structures'] = (val_temp1['structure'], val_temp2['structure'])
+
+    #             key = (j, k)
+    #             if key in coors_Li_dist_structures:
+    #                 coors_Li_dist_structures[key].append(coors_Li_dist_structures_dict)
+    #             else:
+    #                 coors_Li_dist_structures[key] = coors_Li_dist_structures_dict               
+
+    #     sorted_coors_Li_dist_structures = dict(sorted(coors_Li_dist_structures.items(), key=lambda item: item[1]['dist']))
+    #     dist_ascending = list({val['dist'] for idx, val in sorted_coors_Li_dist_structures.items()})
+
+    #     df_dist_litoli.at[index, "dist"] = dist_ascending[1:6]
+
+    #     # df_coor.at[i, col_xyz_coor] = coor_Li
+
+    # #     # # for j in range(len(coor_Li)):
+    # #     # #     # df_distance.at[i, f"{j}"] = None  
+
+    # #     # #     distance = mic_eucledian_distance(coor_Li_ref[j], coor_Li[j])
+
+    # #     # #     df_distance.at[i, f"{j}"] = distance
+
+    # #     # #     diameter_24g48h = max_mapping_radius * 2
+    # #     # #     if distance < diameter_24g48h and i != 0:
+    # #     # #         print(f"path: {i}, Li: {j}, distance: {distance}")
+
+    return df_distance
+
+
