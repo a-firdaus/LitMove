9e77a3d41c1b9bf4a941bf31c9370146abd95d83
Azka Firdaus <azka.firdaus@rwth-aachen.de>
2024-05-25 19:42:23 +0200

    attempt to save movement plots as pdf in each chunk

--- positionism/plot/movement.py
+++ positionism/plot/movement.py
@@ -453,12 +453,16 @@ def plot_occupancy(dataframe, sorted, direc_restructure_destination, litype, str
 # # # #     plt.show()
 
 
-def plot_cage_tuple_label(df_distance, df_type, df_idx_tuple, max_mapping_radius, activate_diameter_line, activate_relabel_s_i, Li_idxs):
+def plot_cage_tuple_label(df_distance, df_type, df_idx_tuple, direc_restructure_destination, max_mapping_radius, litype, activate_diameter_line, activate_relabel_s_i, Li_idxs):
     category_labels = {
         '48htype2': '48h type 1',
         '48htype1': '48h type 2',
         '48htype3': '48h type 3',
         '48htype4': '48h type 4',
+        '48htype5': '48h type 5',
+        '48htype6': '48h type 6',
+        '48htype7': '48h type 7',
+        '48htype8': '48h type 8',
         '24g': '24g',
         'weirdo': 'Unassigned'
         # ... add more as needed
@@ -646,4 +650,216 @@ def plot_cage_tuple_label(df_distance, df_type, df_idx_tuple, max_mapping_radius
     # Enable zooming with cursor
     mpldatacursor.datacursor(display='multiple', draggable=True)
 
+    plt.tight_layout()
+    if Li_idxs == "all":
+        plt.savefig(f"{direc_restructure_destination}/cage_tuple_label_plot_litype{litype}_all.pdf", format='pdf')
+    else:
+        plt.savefig(f"{direc_restructure_destination}/cage_tuple_label_plot_litype{litype}_{i}.pdf", format='pdf')
+        
+    plt.show()
+
+
+def plot_cage_tuple_label_edited(df_distance, df_type, df_idx_tuple, direc_restructure_destination, max_mapping_radius, litype, activate_diameter_line, activate_relabel_s_i, Li_idxs):
+    category_labels = {
+        '48htype2': '48h type 1',
+        '48htype1': '48h type 2',
+        '48htype3': '48h type 3',
+        '48htype4': '48h type 4',
+        '48htype5': '48h type 5',
+        '48htype6': '48h type 6',
+        '48htype7': '48h type 7',
+        '48htype8': '48h type 8',
+        '24g': '24g',
+        'weirdo': 'Unassigned'
+        # ... add more as needed
+    }    
+
+    # df_distance = df_distance.iloc[:,:amount_Li]
+    # df_type = df_type.iloc[:,:amount_Li]
+    # df_idx_tuple = df_idx_tuple.iloc[:,:amount_Li]
+
+    diameter_24g48h = max_mapping_radius * 2
+
+    x = range(len(df_distance))
+
+    # # fig = plt.figure()
+    # fig = plt.figure(figsize=(800/96, 600/96))  # 800x600 pixels, assuming 96 DPI
+    # ax = plt.subplot(111)
+
+    # type_marker_mapping = {
+    #     '48htype1': 'o',
+    #     '48htype2': 's',
+    #     '48htype3': '^',
+    #     '48htype4': 'D',
+    #     'weirdos': 'X',
+    #     '24g': 'v'    
+    # }
+
+    # # type_marker_mapping = {
+    # #     '48htype1': ('o', 'r'),  # Example: Circle marker with red color
+    # #     '48htype2': ('s', 'g'),  # Square marker with green color
+    # #     '48htype3': ('^', 'b'),  # Triangle marker with blue color
+    # #     '48htype4': ('D', 'c'),  # Diamond marker with cyan color
+    # #     'weirdos': ('X', 'm'),   # X marker with magenta color
+    # #     '24g': ('v', 'y')        # Triangle_down marker with yellow color
+    # # }
+
+    fig, ax = plt.subplots(figsize=(10, 6))  # Set the figure size in inches
+
+    lines = []
+    texts = []
+    
+    type_marker_mapping = {
+        '48htype1': ('o'),  # Example: Circle marker with red color
+        '48htype2': ('s'),  # Square marker with green color
+        '48htype3': ('^'),  # Triangle marker with blue color
+        '48htype4': ('D'),  # Diamond marker with cyan color
+        'weirdos': ('X'),   # X marker with magenta color
+        '24g': ('v')        # Triangle_down marker with yellow color
+    }
+
+    colors = ['b', 'g', 'r', 'c', 'm', 'y'] #, 'k']  # Example color list
+    # colors = list(mcolors.CSS4_COLORS.values())
+    # colors = [color + (0.7,) for color in mcolors.CSS4_COLORS.values()]
+    # colors = mcolors
+    # names = list(colors)
+
+    # Define offsets for text position
+    x_offset = 0.02  # Adjust these values as needed
+    y_offset = -0.05  # Adjust these values as needed
+
+    # Track which labels have been added
+    added_labels = set()
+
+    # for i in range(24):
+    for i in range(len(df_distance.columns)):
+        if Li_idxs == "all" or i in Li_idxs:
+            column_data = df_distance[f"{i}"]
+            column_val = df_type[f"{i}"]
+            column_idx_tuple = df_idx_tuple[f"{i}"]
+            # type_val = df_type[0, i]
+            # print(type_val)
+
+            line_color = colors[i % len(colors)]  # Cycle through colors list
+            # # # # # # # line_color = colors[i % len(colors)] if i < len(colors) else 'black'  # Use a default color if the index exceeds available colors
+
+            # # # for j in x:
+            for j, (y_val, type_val, idx_tuple_val) in enumerate(zip(column_data, column_val, column_idx_tuple)):
+                # type = column_val[j]
+                # idx_tuple = column_idx_tuple[j]
+
+                # marker_style = type_marker_mapping.get(column_val, 'o')  # Get the marker style for the type
+                # # marker_style = type_marker_mapping.get(type, 'o')  # Get the marker style for the type
+                # # # # # # # marker_style, marker_color = type_marker_mapping.get(type_val, ('o','k'))  # Get the marker style for the type
+                marker_style = type_marker_mapping.get(type_val, ('o'))  # Get the marker style for the type
+                # # # # # # ax.scatter(j, df_distance[f"{i}"][j], label=f"Type: {column_val}", marker=marker_style, s=100)
+                # # # # # label = f"{type_val}" if type_val not in added_labels else None
+                # # # # # # # ax.scatter(j, df_distance.iloc[j, i], label=label, marker=marker_style, s=100)
+                # # # # # # # # # ax.scatter(j, df_distance.iloc[j, i], label=label, marker=marker_style, s=100, color = marker_color, alpha = 0.5)
+                # # # # # ax.scatter(j, y_val, label=label, marker=marker_style, s=100, color = marker_color, alpha = 0.5)
+                # # # # # added_labels.add(type_val)
+                mapped_label = category_labels.get(type_val, type_val)  # Use the original type_val if it's not found in category_labels
+                # Use mapped_label for the label. Only add it if it's not already added.
+                label = mapped_label if mapped_label not in added_labels else None
+                ax.scatter(j, y_val, label=label, marker=marker_style, s=100, color=line_color, alpha=0.5)
+                if label:  # If a label was added, record it as added
+                    added_labels.add(mapped_label)
+
+                # # # # ax.text(j, df_distance.iloc[j, i], str(int(idx_tuple_val)), color=line_color, fontsize=20)
+                # # # # # ax.text(j, y_val, str(int(idx_tuple_val)), color=line_color, fontsize=20)
+                # Apply offsets to text position
+                text_x = j + x_offset * ax.get_xlim()[1]  # Adjust text x-position
+                text_y = y_val + y_offset * ax.get_ylim()[1]  # Adjust text y-position
+
+                # Check if type_val is 'weirdos' and change text color to black, else use the line color
+                text_color = 'black' if type_val in ['weirdos', '48htype1'] else line_color
+
+                if activate_relabel_s_i:        
+                    # print(idx_tuple_val)
+                    if type_val in ['48htype2', '48htype3', '48htype4', '24g']:
+                        text = ax.text(text_x, text_y, str(int(idx_tuple_val))+"s", color=text_color, fontsize=18)
+                    elif type_val in ['48htype1']:
+                        text = ax.text(text_x, text_y, str(int(idx_tuple_val))+"i", color=text_color, fontsize=18)
+                    elif type_val == 'weirdos':
+                        # idx_tuple_val = 'x'
+                        # # print(idx_tuple_val)
+                        text = ax.text(text_x, text_y, idx_tuple_val, color=text_color, fontsize=18)
+                        # # print(text)
+                else:
+                    if idx_tuple_val == 'x':
+                        text = ax.text(text_x, text_y, idx_tuple_val, color=text_color, fontsize=18)
+                    else:
+                        text = ax.text(text_x, text_y, str(int(idx_tuple_val)), color=text_color, fontsize=18)
+                texts.append(text)
+
+                # # # # # # # # # # # # # # text = ax.text(j+x_offset, y_val+y_offset, str(int(idx_tuple_val)), color=line_color, fontsize=15)
+                # # # # # # # # if idx_tuple_val == f' ':
+                # # # # # # # #     text = ax.text(text_x, text_y, idx_tuple_val, color=line_color, fontsize=18)
+                # # # # # # # # else:
+                # # # # # # # #     text = ax.text(text_x, text_y, str(int(idx_tuple_val)), color=line_color, fontsize=18)
+                # # # # # # # # texts.append(text)
+
+            # # i = i
+            # # line, = ax.plot(x, df_distance[f"{i}"], label=f"{i}")
+            # line, = ax.plot(x, df_distance[f"{i}"], label=f"{i}", linewidth=2, marker=marker_style, markersize=10)  # Set line width to 2 pixels
+
+            line, = ax.plot(x, df_distance[f"{i}"], label=f"{i}", color=line_color, linewidth=2)  # Set line width to 2 pixels
+            # ax.text(i, value, str(int(idx_value)), color=line_color, fontsize=8)
+
+            lines.append(line)
+            # label = f"{i}" if Li_idxs == "all" else None
+            # line, = ax.plot(x, df_distance[f"{i}"], label=label)
+            # lines.append(line)
+
+        # if type(Li_idxs) == list:
+        #     for j in Li_idxs:
+        #         if i == j:
+        #             line, = ax.plot(x, df_distance[f"{i}"], label=f"{i}")
+        #             lines.append(line)
+
+    adjust_text(texts, arrowprops=dict(arrowstyle='->', color='red'))
+    
+    # # ax.axhline(y=diameter_24g48h, color='b', linestyle=':', label=f'd_mapping = {diameter_24g48h:.3f}')
+    if activate_diameter_line == True:
+        ax.axhline(y=diameter_24g48h, color='b', linestyle=':', label=f'd_mapping = {diameter_24g48h:.3f}', linewidth=1)  # Set line width to 1 pixel
+
+    # ax.set_ylim(-0.5, 3.5)
+
+    # Set the y-axis to only show ticks at 0, 1, 2, 3
+    plt.yticks([0, 1, 2, 3])
+
+    # plt.title(f"Geometry {geo} with d={diameter_24g48h}")
+
+    # Shrink current axis's height by 10% on the bottom
+        # source: https://stackoverflow.com/questions/4700614/how-to-put-the-legend-outside-the-plot
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0 + box.height * 0.1,
+                    box.width, box.height * 0.9])
+
+    handles, labels = ax.get_legend_handles_labels()
+
+    # Set marker color in legend box to black
+    # # legend_handles = [(h[0], h[1], {'color': 'black'}) for h in handles]
+    legend_handles = [(h, {'color': 'black'}) for h in handles]
+
+    ax.legend(handles=handles, labels=labels, loc='upper center', bbox_to_anchor=(0.5, -0.05),
+            fancybox=True, shadow=True, ncol=5)
+    
+    # # # ax.legend(handles=legend_handles, labels=labels, loc='upper center', bbox_to_anchor=(0.5, -0.05),
+    # # #         fancybox=True, shadow=True, ncol=5, handlelength=2, handler_map={tuple: HandlerTuple(ndivide=None)})
+    # ax.legend(handles=legend_handles, labels=labels, loc='upper center', bbox_to_anchor=(0.5, -0.05),
+    #         fancybox=True, shadow=True, ncol=5)
+
+    # Enable cursor information
+    mplcursors.cursor(hover=True)
+
+    # Enable zooming with cursor
+    mpldatacursor.datacursor(display='multiple', draggable=True)
+
+    plt.tight_layout()
+    if Li_idxs == "all":
+        plt.savefig(f"{direc_restructure_destination}/cage_tuple_label_plot_litype{litype}_all.pdf", format='pdf')
+    else:
+        plt.savefig(f"{direc_restructure_destination}/cage_tuple_label_plot_litype{litype}_{Li_idxs[0]}.pdf", format='pdf')
+        
     plt.show()
